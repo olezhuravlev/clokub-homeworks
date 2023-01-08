1becc9039c8bf92a2ec364d9a349afb82dd1e43b
Oleg Zhuravlev <olezhuravlev@gmail.com>
2023-01-08 17:10:11 +0700

    15.4 done

---
+++ 15.4/terraform/01_provider.tf
@@ -0,0 +1,21 @@
+# Base Terraform provider definition.
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+# Define provider.
+provider "yandex" {
+  cloud_id = var.yandex-cloud-id
+  zone     = var.yandex-cloud-zone1
+}
+
+# Create folder.
+resource "yandex_resourcemanager_folder" "application-folder" {
+  cloud_id = var.yandex-cloud-id
+  name     = "application-folder"
+}
